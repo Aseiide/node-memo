3afff37cf3cce13161954563361997c7ed55818a
Aseiide <eiseiide@gmail.com>
2021-09-22 18:00:29 +0900

    WIP

--- memo.js
+++ memo.js
@@ -11,7 +11,7 @@ const db = new sqlite3.Database(dbname)
 
 db.serialize(() => {
   db.run('DROP TABLE IF EXISTS memos')
-  db.run('CREATE TABLE IF NOT EXISTS memos(id integer, title text, body, text)')
+  db.run('CREATE TABLE IF NOT EXISTS memos(id INTEGER PRIMARY KEY autoincrement, title text, body, text)')
   db.run('INSERT INTO memos(id, title, body) values(?, ?, ?)', 1, 'test1', 'バナナ\nトマト\n牛乳\n')
   db.run('INSERT INTO memos(id, title, body) values(?, ?, ?)', 2, 'test2', 'ティッシュ\n洗剤')
   db.run('INSERT INTO memos(id, title, body) values(?, ?, ?)', 3, 'test3', 'ヨーグルト')
@@ -21,7 +21,7 @@ db.serialize(() => {
   })
 })
 
-// DBに保存されたタイトルを取り出してconsole.logで出力する
+// 一覧機能: DBに保存されたタイトルを取り出してconsole.logで出力する
 db.serialize(() => {
   db.all('SELECT title FROM memos', function (err, rows) {
     if (err) {
@@ -32,5 +32,30 @@ db.serialize(() => {
     })
   })
 })
+
+// 参照機能: 保存されたタイトルを取り出してconsole.logに出力
+// それを選択できるようにする
+db.serialize(() => {
+  db.all('SELECT title FROM memos', function (err, rows) {
+    if (err) {
+      throw err;
+    }
+    rows.forEach(function (row) {
+      console.log(row.title)
+    })
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 db.close()
